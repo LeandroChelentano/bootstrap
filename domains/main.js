64f7dfee1a4ed7df9bb1588c06a8adafc8ce8b3d
Leandro Chelentano <lchelentano@hotmail.com>
2021-08-18 18:55:17 -0300

    v.1.1

---
+++ domains/main.js
@@ -0,0 +1,7 @@
+let toggleFilter = () => {
+    if (document.getElementById('filterMobile').style.display == 'none') {
+        document.getElementById('filterMobile').style.display = 'block'
+    } else {
+        document.getElementById('filterMobile').style.display = 'none'
+    }
+}
