dfd8cb5485acf62addf9ef4c0e66ede6a5efdaa5
Leandro Chelentano <lchelentano@hotmail.com>
2021-08-21 20:49:46 -0300

    v1.3

---
+++ domains/data.js
@@ -0,0 +1,85 @@
+const articulos = [{
+    id: 0,
+    name: "Chaque Azul Marino",
+    desc: "Ropa para hombre",
+    img: "../imgs/clothes/Chaque-Azul-Marino-Chaleco-Recto.jpg",
+    gender: "M",
+    price: 135
+},{
+    id: 1,
+    desc: "Ropa para hombre",
+    name: "Chaque Azul Tinta",
+    img: "../imgs/clothes/Chaque-Azul-Tinta-Chaleco-Recto.jpg",
+    gender: "M",
+    price: 135
+},{
+    id: 2,
+    desc: "Ropa para hombre",
+    name: "Traje Azul Marino",
+    img: "../imgs/clothes/Traje-Azul-Marino-Camisa-Blanca.jpg",
+    gender: "M",
+    price: 120 
+},{
+    id: 3,
+    desc: "Ropa para hombre",
+    name: "Traje Azul Tinta",
+    img: "../imgs/clothes/Traje-Azul-Tinta-Corbata-Azul-2.jpg",
+    gender: "M",
+    price: 120
+},{
+    id: 4,
+    desc: "Ropa para hombre",
+    name: "Traje Gris Marengo",
+    img: "../imgs/clothes/Traje-Gris-Marengo-Camisa-Blanca.jpg",
+    gender: "M",
+    price: 120
+},{
+    id: 5,
+    desc: "Ropa para hombre",
+    name: "Traje Negro",
+    img: "../imgs/clothes/Traje-Negro-Camisa-Blanca.jpg",
+    gender: "M",
+    price: 120
+},{ //Mujeres
+    id: 6,
+    name: "Rib Knit Dress Black",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Rib-knit-Dress-Black.png",
+    gender: "F",
+    price: 100
+},{
+    id: 7,
+    name: "Rib Knit Dress",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Rib-knit-Dress.png",
+    gender: "F",
+    price: 120
+},{
+    id: 8,
+    name: "Rib Knit Pants",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Rib-knit-Pants.png",
+    gender: "F",
+    price: 90
+},{
+    id: 9,
+    name: "Ribbed Jersey Dress",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Ribbed Jersey Dress.png",
+    gender: "F",
+    price: 95
+},{
+    id: 10,
+    name: "Ribbed Dress",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Ribbed-Dress.png",
+    gender: "F",
+    price: 130
+},{
+    id: 11,
+    name: "Ribbed Sweater Vest",
+    desc: "Ropa de mujer",
+    img: "../imgs/clothes/Ribbed-Sweater-Vest.png",
+    gender: "F",
+    price: 90
+}];
--- domains/main.js
+++ domains/main.js
@@ -1,7 +1,120 @@
 let toggleFilter = () => {
-    if (document.getElementById('filterMobile').style.display == 'none') {
-        document.getElementById('filterMobile').style.display = 'block'
+  if (document.getElementById("filterMobile").style.display == "none") {
+    document.getElementById("filterMobile").style.display = "block";
+  } else {
+    document.getElementById("filterMobile").style.display = "none";
+  }
+};
+
+let goTo = (artId) => {
+  window.localStorage.setItem("identifier", artId);
+  window.open("articulo.html", "_self");
+};
+
+let loadArticle = () => {
+  var artId = window.localStorage.getItem("identifier");
+  artId = parseInt(artId);
+
+  let tit = document.getElementById("title");
+  let des = document.getElementById("desc");
+  let imm = document.getElementById("image");
+  let pri = document.getElementById("price");
+
+  tit.innerHTML = articulos[artId].name;
+  des.innerHTML = articulos[artId].desc;
+  imm.src = articulos[artId].img;
+  pri.innerHTML = `U$S ${articulos[artId].price}`;
+
+  tit.className = "artTitle";
+  des.className = "artDesc";
+  imm.className = "artImg";
+  pri.className = "artPrice";
+
+  document.title = `${articulos[artId].name} - Indumentis`;
+};
+
+let loadMens = () => {
+  for (let k = 0; k < 4; k++) {
+    for (let i = 0; i < articulos.length; i++) {
+      if (articulos[i].gender == "M") {
+        let articleElement = document.createElement("article");
+        articleElement.id = articulos[i].id;
+
+        articleElement.setAttribute("onclick", "goTo(this.id)");
+
+        let img = document.createElement("img");
+        img.src = articulos[i].img;
+
+        let sep = document.createElement("div");
+        sep.className = "separator";
+
+        let artName = document.createElement("p");
+        artName.innerHTML = articulos[i].name;
+        artName.className = "artTitle";
+
+        let artPrice = document.createElement("p");
+        artPrice.innerHTML = `U$S ${articulos[i].price}`;
+        artPrice.className = "artPrice";
+
+        articleElement.appendChild(img);
+        articleElement.appendChild(sep);
+        articleElement.appendChild(artName);
+        articleElement.appendChild(artPrice);
+
+        document.getElementById("articlesHolder").appendChild(articleElement);
+      }
+    }
+  }
+};
+
+let loadWomans = () => {
+  for (let k = 0; k < 4; k++) {
+    for (let i = 0; i < articulos.length; i++) {
+      if (articulos[i].gender == "F") {
+        let articleElement = document.createElement("article");
+        articleElement.id = articulos[i].id;
+
+        articleElement.setAttribute("onclick", "goTo(this.id)");
+
+        let img = document.createElement("img");
+        img.src = articulos[i].img;
+
+        let sep = document.createElement("div");
+        sep.className = "separator";
+
+        let artName = document.createElement("p");
+        artName.innerHTML = articulos[i].name;
+        artName.className = "artTitle";
+
+        let artPrice = document.createElement("p");
+        artPrice.innerHTML = `U$S ${articulos[i].price}`;
+        artPrice.className = "artPrice";
+
+        articleElement.appendChild(img);
+        articleElement.appendChild(sep);
+        articleElement.appendChild(artName);
+        articleElement.appendChild(artPrice);
+
+        document.getElementById("articlesHolder").appendChild(articleElement);
+      }
+    }
+  }
+};
+
+let toggleFrm = () => {
+    let frm = document.getElementById('frm');
+    let btn = document.getElementById('reservar');
+    let wait = document.getElementById('waitingHolder');
+
+    if (frm.style.display == "none") {
+        frm.style.display = "block";
+        wait.style.display = "none";
+        btn.innerHTML = "Cancelar";
+        btn.className = "btn btn-danger";
     } else {
-        document.getElementById('filterMobile').style.display = 'none'
+        frm.style.display = "none";
+        wait.style.display = "block";
+        btn.innerHTML = "Reservar";
+        btn.className = "btn btn-primary";
     }
 }
